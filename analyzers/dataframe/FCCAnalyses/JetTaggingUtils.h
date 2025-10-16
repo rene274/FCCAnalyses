b7b5b6963aab5c0158cc9eca6c14d9c342f6ae1e
rene274 <rene.kootz@desy.de>
2025-10-16 14:00:45 +0200

    added get_Zqq_flavour
    copied from Kunal Gautam:
    https://github.com/kunal2796/FCCAnalyses/blob/asymmetries/analyzers/dataframe/FCCAnalyses/JetTaggingUtils.h

--- analyzers/dataframe/FCCAnalyses/JetTaggingUtils.h
+++ analyzers/dataframe/FCCAnalyses/JetTaggingUtils.h
@@ -52,7 +52,10 @@ struct sel_tag {
              ROOT::VecOps::RVec<fastjet::PseudoJet> in);
 };
 
-///@}
+/////// added functions ////////
+//Makeshift way to get Z flavour
+ROOT::VecOps::RVec<int> get_Zqq_flavour(ROOT::VecOps::RVec<fastjet::PseudoJet> in, ROOT::VecOps::RVec<edm4hep::MCParticleData> MCin);
+
 } // namespace JetTaggingUtils
 
 } // namespace FCCAnalyses
